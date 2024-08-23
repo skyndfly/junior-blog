53d038b7940555335381b982d6820cb9458642aa
skyandfly <skyandy2796@yandex.ru>
2024-08-23 11:39:33 +0300

    feat: add google yandex verification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,8 @@
           content="Junior Blog - Ваш гид в мире PHP, Laravel и Yii2. Узнайте о решении повседневных задач программиста, профессиональном росте и обучении. Полезные статьи и советы для программистов всех уровней.">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="keywords" content="PHP, Laravel, Yii2, программирование, обучение, профессиональный рост, задачи программиста">
-
+    <meta name="yandex-verification" content="99fa8bc0aaba3590" />
+    <meta name="google-site-verification" content="0hcR8Eq0mO6ItZ5HZBze57apLJB-b0qzXTli5M0TfV4" />
     <meta property="og:title" content="Junior Blog - Программирование на PHP, Laravel, Yii2">
     <meta property="og:description" content="Ваш гид в мире PHP, Laravel и Yii2. Узнайте о решении повседневных задач программиста, профессиональном росте и обучении. Полезные статьи и советы для программистов всех уровней.">
     <meta property="og:url" content="https://junior-blog.ru/">
